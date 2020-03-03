b25b0aee63b4a89987f0a159944d244c163f46d4
Chandan Panigrahi <cpanigrahiofficial99@gmail.com>
2020-03-03 10:43:52 -0800

    Add files via upload

---
+++ IrisFlower.py
@@ -0,0 +1,63 @@
+# Load libraries
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+# Loading the dataset
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+# Multivariate plot of the dataset 
+scatter_matrix(dataset)
+pyplot.show()
+# Creating training and validation data set
+array = dataset.values
+X = array[:,0:4]
+Y = array[:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=0.20, random_state=1)
+# Implementing the models and comparing them to find the best suitable
+models=[]
+models.append(('LR', LogisticRegression(solver='liblinear',multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+results=[]
+names=[]
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results= cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    print('%s: %f (%f)' %(name, cv_results.mean(), cv_results.std()))
+    
+# Boxplot comparison
+pyplot.boxplot(results ,labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
+
+# Implementing the best fit model on training data set
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+
+# Prediction of the results
+predictions = model.predict(X_validation)
+print(predictions)
+# prediction list for first 4 validation data
+predictionsFour = model.predict(X_validation[0:4])
+print("the prediction for first four validation data is",predictionsFour)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
